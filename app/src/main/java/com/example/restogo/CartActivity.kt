7391b677e4e337996d1f53327202b3abb4bf5ffe
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-06-06 23:15:08 +0800

    feat: CartActivity [layout] --done for frontend with recycleview application

--- app/src/main/java/com/example/restogo/CartActivity.kt
+++ app/src/main/java/com/example/restogo/CartActivity.kt
@@ -1,11 +1,46 @@
 package com.example.restogo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class CartActivity : AppCompatActivity() {
+class CartActivity : Activity(), View.OnClickListener {
+    private lateinit var btnBack: ImageView
+    private lateinit var recycleView: RecyclerView
+    private lateinit var edtCoupon: EditText
+    private lateinit var btnApply: Button
+    private lateinit var btnKirim: Button
+    private lateinit var tvTotalPrice: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        initComponents()
+
+        btnBack.setOnClickListener(this)
+    }
+
+    private fun initComponents() {
+        btnBack = findViewById(R.id.img_cart_back)
+        recycleView = findViewById(R.id.rv_cart)
+        edtCoupon = findViewById(R.id.edt_cart_coupon)
+        btnApply = findViewById(R.id.btn_cart_apply)
+        btnKirim = findViewById(R.id.btn_cart_kirim)
+        tvTotalPrice = findViewById(R.id.tv_cart_total_price)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.img_cart_back) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
