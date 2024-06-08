f38fda0a24affd0b590e9913cff38e9288c63bd0
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-06-08 23:56:38 +0800

    feat: DetailMenuActivity [layout] --adding layout for detail menu, but radio button not responsive, to do next

--- app/src/main/java/com/example/restogo/DetailMenuActivity.kt
+++ app/src/main/java/com/example/restogo/DetailMenuActivity.kt
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DetailMenuActivity : Activity() {
+
+
+    companion object {
+        const val EXTRA_MENU: String = "extra_menu"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_menu)
