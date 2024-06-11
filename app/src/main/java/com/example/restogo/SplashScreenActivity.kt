cf8f45468b7ad9313234622ed14a358f729edbad
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-06-11 14:22:10 +0800

    fix: SplashScreenActivity [layout] --remove the button & adding thread

--- app/src/main/java/com/example/restogo/SplashScreenActivity.kt
+++ app/src/main/java/com/example/restogo/SplashScreenActivity.kt
@@ -2,34 +2,27 @@ package com.example.restogo
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 
-class SplashScreenActivity : Activity(), View.OnClickListener {
-    private lateinit var btnPesan: Button
+class SplashScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        initComponents()
-        btnPesan.setOnClickListener(this)
-    }
 
-    private fun initComponents() {
-        btnPesan = findViewById(R.id.btn_splash_pesan)
-    }
-
-    override fun onClick(v: View?) {
-        if (v?.id == R.id.btn_splash_pesan) {
+        val thread = Thread {
             try {
-                val intent = Intent(this, ChoiceRoleActivity::class.java)
-                startActivity(intent)
+                // Tunda eksekusi selama 2000 milidetik (2 detik)
+                Thread.sleep(2000)
             } catch (e: InterruptedException) {
                 e.printStackTrace()
             } finally {
+                // Setelah penundaan, buka MainActivity
+                startActivity(Intent(this, MainActivity::class.java))
+                // Hentikan SplashActivity agar tidak kembali saat tombol kembali ditekan dari MainActivity
                 finish()
             }
         }
+        // Mulai eksekusi thread
+        thread.start()
     }
 }
