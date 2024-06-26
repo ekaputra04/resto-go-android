126c39981c29f93c3730a963e645037b05e7a1a7
sultanazizul <sultanazizulh@gmail.com>
2024-06-26 19:19:51 +0800

    aborting

--- app/src/main/java/com/example/restogo/Env.kt
+++ app/src/main/java/com/example/restogo/Env.kt
@@ -5,7 +5,7 @@ package com.example.restogo
 object Env {
 //    val dotenv = dotenv()
 
-    //val apiUrl = "http://10.0.2.2:8000"
-    val apiUrl ="https://backend-resto-go.vercel.app/"
+    val apiUrl = "http://10.0.2.2:8000"
+    // val apiUrl ="https://backend-resto-go.vercel.app/"
     val apiKey = "restogo"
 }
