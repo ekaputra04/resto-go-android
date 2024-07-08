e8cb04c20743368c22c27cc302cafef42f319f74
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-07-08 22:16:32 +0800

    feat: SuccessOrderActivity [layout] --add the layout

---
+++ app/src/main/java/com/example/restogo/SuccessOrderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.restogo
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuccessOrderActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success_order)
+    }
+}
